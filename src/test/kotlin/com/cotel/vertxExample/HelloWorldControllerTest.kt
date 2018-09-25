bd96cb980e2a3c1aaf909408879ffcbd730eee74
Cotel <miguelcoletomunoz@hotmail.com>
2018-09-25 20:39:03 +0200

    Adding a test for HelloWorldController

--- src/test/kotlin/com/cotel/vertxExample/HelloWorldControllerTest.kt
+++ src/test/kotlin/com/cotel/vertxExample/HelloWorldControllerTest.kt
@@ -4,15 +4,15 @@ import io.vertx.core.Vertx
 import io.vertx.junit5.Timeout
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import java.util.concurrent.TimeUnit
 
 @ExtendWith(VertxExtension::class)
-class TestMainVerticle {
+class HelloWorldControllerTest {
 
   @BeforeEach
   fun deploy_verticle(vertx: Vertx, testContext: VertxTestContext) {
@@ -20,15 +20,14 @@ class TestMainVerticle {
   }
 
   @Test
-  @DisplayName("Should start a Web Server on port 8080")
-  @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
+  @Timeout(value = 20, timeUnit = TimeUnit.SECONDS)
   @Throws(Throwable::class)
-  fun start_http_server(vertx: Vertx, testContext: VertxTestContext) {
+  fun `should show hello world phrase when requesting GET hello`(vertx: Vertx, testContext: VertxTestContext) {
     vertx.createHttpClient().getNow(8080, "localhost", "/") { response ->
       testContext.verify {
-        assertTrue(response.statusCode() == 200)
+        assertEquals(200, response.statusCode())
         response.handler { body ->
-          assertTrue(body.toString().contains("Hello from Vert.x!"))
+          assertTrue(body.toString().contains("Hello world frin Vertx!"))
           testContext.completeNow()
         }
       }
